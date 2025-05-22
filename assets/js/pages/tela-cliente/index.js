9469051bef302f253d55997469fa74fcdca1e9d1
Cauã Felipe <devcaua098@gmail.com>
2025-05-22 20:19:55 -0300

    Zapfood_1.3

--- assets/js/pages/tela-cliente/index.js
+++ assets/js/pages/tela-cliente/index.js
@@ -1,20 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Código para o menu de cadastro
-    const cadastrarBtn = document.getElementById('cadastrarBtn');
-    const cadastroOpcoes = document.getElementById('cadastroOpcoes');
-
-    if (cadastrarBtn && cadastroOpcoes) {
-        cadastrarBtn.addEventListener('click', function(event) {
-            event.preventDefault();
-
-            if (cadastroOpcoes.style.display === 'block') {
-                cadastroOpcoes.style.display = 'none';
-            } else {
-                cadastroOpcoes.style.display = 'block';
-            }
-        });
-    }
-
     // Código para a rolagem ao clicar no botão de pedido
     const botaoPedido = document.querySelector('.botao-pedido');
     const secaoPratos = document.querySelector('.sessao:nth-child(1)');
